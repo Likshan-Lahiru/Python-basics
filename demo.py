bb48e0d1c571cfa28e8e1bdfab49e936055686bf
lahiruileperumage <lahiru212001@gmail.com>
2024-05-06 19:39:41 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+
+print(1234)
+print('Hello..')
+print("Strings")
+print(True)
+print(2 + 3j)
+
+
+
+print("Hello \n world")
+print("name\t: lahiru")
+print("Country\t: Sri lanka")
+
