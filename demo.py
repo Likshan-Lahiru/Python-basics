4c814bc7c666512f0b844e68b8886dd2145df829
lahiruileperumage <lahiru212001@gmail.com>
2024-05-06 20:12:24 +0530

    python operates

--- demo.py
+++ demo.py
@@ -1,15 +1,44 @@
+#how  python print some value on terminal
 print("Hello world")
 
 
-print(1234)
-print('Hello..')
-print("Strings")
-print(True)
-print(2 + 3j)
+#python data types
+print(1234)#int
+print('Hello..')#Strings
+print("Strings")#Strings
+print(True)#boolean
+print(2 + 3j)#advance integer
+
+
+#escape characters 
+print("Hello \n world")#new line 
+
+print("name\t: lahiru")#tab space
+print("Country\t: Sri lanka")#tab space
+
+
+#python variable
+name  = "Lahiru"
+
+print(name)
+
+x = 1
+
+y = 2
+
+z = x + y
+
+print(z) 
+
+print(10**3)
+
+print();
 
 
 
-print("Hello \n world")
-print("name\t: lahiru")
-print("Country\t: Sri lanka")
+
+
+
+
+
 
